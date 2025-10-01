1d4f0da901e5810de64c4acaa4e566a445d4af18
Undefined <heleri.mandla@gmail.com>
2025-10-01 14:07:40 +0300

    feat(vertical-stepper): design changes and additional slot for info components #512 (#1131)
    * feat(vertical-stepper): design changes #512
    
    * feat(vertical-stepper): remove typo from description #512
    
    * feat(vertical-stepper): design changes #512
    
    * feat(vertical-stepper): design changes #512
    
    ---------
    
    Co-authored-by: Heleri Mändla <Heleri.Mandla@twn.ee>

--- libs/react-components/src/community/components/vertical-stepper/step-item/step-item.tsx
+++ libs/react-components/src/community/components/vertical-stepper/step-item/step-item.tsx
@@ -1,13 +1,14 @@
 import cn from 'classnames';
 import React from 'react';
 
+import { Col, Row } from '../../../../tedi';
 import { Icon } from '../../../../tedi/components/base/icon/icon';
 import Collapse from '../../../../tedi/components/buttons/collapse/collapse';
 import styles from '../vertical-stepper.module.scss';
 
 export interface StepItemProps {
   /**
-   * SubItems and additional info components like StatusBadge, Button, Link or Text.
+   * SubItems
    */
   children?: React.ReactNode;
   /**
@@ -21,10 +22,13 @@ export interface StepItemProps {
   /**Step title text */
   title: string | React.ReactNode;
   href?: string;
-  /** */
   state?: 'default' | 'completed' | 'error' | 'disabled';
   hasIcon?: boolean;
   onClick?: () => void;
+  /**
+   * Additional info components like StatusBadge, Button, Link or Text.
+   */
+  info?: React.ReactNode;
 }
 
 export const StepItem = ({
@@ -36,12 +40,17 @@ export const StepItem = ({
   href,
   onClick,
   state = 'default',
+  info,
 }: StepItemProps): JSX.Element => {
+  const [open, selected] = React.useState(false);
   const stepItemClassName = cn(
     styles['stepper-item'],
     {
       [styles[state]]: state,
       [styles['selected']]: isSelected,
+      [styles['has-info']]: info && !children,
+      [styles['info-with-children']]: info && children,
+      [styles['stepper-collapse-open']]: open,
     },
     className
   );
@@ -52,55 +61,68 @@ export const StepItem = ({
       <div className={styles['stepper-content']}>
         {children ? (
           <Collapse
+            onToggle={(isOpen) => selected(isOpen)}
+            open={open}
             hideCollapseText
             id="vertical-stepper-collapse"
+            className={styles['stepper-item-collapse']}
             title={
-              <span className={styles['stepper-link']}>
-                {title}
-                {hasIcon && state === 'error' && (
-                  <Icon
-                    name="error"
-                    color="danger"
-                    size={16}
-                    display="inline"
-                    className={styles['radio__tooltip-icon']}
-                  />
-                )}
-
-                {hasIcon && state === 'completed' && (
-                  <Icon
-                    name="check"
-                    color="success"
-                    size={16}
-                    display="inline"
-                    className={styles['radio__tooltip-icon']}
-                  />
-                )}
-              </span>
+              <>
+                <Row alignItems="start">
+                  <Col className={styles['stepper-link']}>
+                    {title}
+                    {hasIcon && state === 'error' && (
+                      <Icon
+                        name="error"
+                        color="danger"
+                        size={16}
+                        display="inline"
+                        className={styles['radio__tooltip-icon']}
+                      />
+                    )}
+                    {hasIcon && state === 'completed' && (
+                      <Icon
+                        name="check"
+                        color="success"
+                        size={16}
+                        display="inline"
+                        className={styles['radio__tooltip-icon']}
+                      />
+                    )}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>{info}</Col>
+                </Row>
+              </>
             }
           >
             {children && <ul className={styles['sub-item-list']}>{children}</ul>}
           </Collapse>
         ) : (
-          <div className={styles['stepper-link-container']}>
-            <a
-              href={href}
-              onClick={(e) => {
-                e.preventDefault();
-                if (onClick) {
-                  onClick();
-                }
-              }}
-              className={styles['stepper-link']}
-            >
-              {title}
-            </a>
-            <span className={styles['stepper-link-icon']}>
-              {hasIcon && state === 'error' && <Icon name="error" color="danger" size={16} display="inline" />}
-
-              {hasIcon && state === 'completed' && <Icon name="check" color="success" size={16} display="inline" />}
-            </span>
-          </div>
+          <>
+            <div className={styles['stepper-link-container']}>
+              <a
+                href={href}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (onClick) {
+                    onClick();
+                  }
+                }}
+                className={styles['stepper-link']}
+              >
+                {title}
+              </a>
+              <span className={styles['stepper-link-icon']}>
+                {hasIcon && state === 'error' && <Icon name="error" color="danger" size={16} display="inline" />}
+                {hasIcon && state === 'completed' && <Icon name="check" color="success" size={16} display="inline" />}
+              </span>
+            </div>
+            <Row>
+              <Col>{info}</Col>
+            </Row>
+          </>
         )}
       </div>
       <div className={styles['stepper-line']}></div>
