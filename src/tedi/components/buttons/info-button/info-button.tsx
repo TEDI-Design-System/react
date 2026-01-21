4c5fc736a5f40f156ef8d6a6dd65a05b7e29caf1
Airike Jaska <95303654+airikej@users.noreply.github.com>
2026-01-21 12:05:54 +0200

    feat(info-button): added inverted variant and new color prop #420

--- src/tedi/components/buttons/info-button/info-button.tsx
+++ src/tedi/components/buttons/info-button/info-button.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Button, ButtonProps } from '../button/button';
 import styles from './info-button.module.scss';
 
-export interface InfoButtonProps extends Omit<ButtonProps, 'size' | 'children'> {
+export interface InfoButtonProps extends Omit<ButtonProps, 'size' | 'children' | 'color'> {
   /**
    * If true, applies a small size to the InfoButton.
    * @default false
@@ -14,17 +14,23 @@ export interface InfoButtonProps extends Omit<ButtonProps, 'size' | 'children'> 
    * Children elements to be rendered inside the InfoButton.
    */
   children?: React.ReactNode;
+  /*
+   * Color variant of the InfoButton.
+   * @default 'default'
+   */
+  color?: 'default' | 'inverted';
 }
 
 export const InfoButton = React.forwardRef<HTMLButtonElement, InfoButtonProps>(
-  ({ isSmall, children, ...props }, ref): JSX.Element => (
+  ({ isSmall, children, color = 'default', ...props }, ref): JSX.Element => (
     <Button
       className={cn(styles['tedi-info-button'])}
+      data-variant={color === 'inverted' ? 'inverted' : undefined}
       data-name="info-button"
       {...props}
-      type="button"
       icon={{ name: 'info', size: isSmall ? 16 : 18 }}
       visualType="neutral"
+      color={color}
       ref={ref}
     >
       {children}
