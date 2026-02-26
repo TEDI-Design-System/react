ebb8375cee1c3914b65ae9d18aec9207165f921a
Airike Jaska <95303654+airikej@users.noreply.github.com>
2026-02-26 08:04:28 +0200

    chore: fix bootstrap typo #526

--- src/tedi/components/layout/grid/col.stories.tsx
+++ src/tedi/components/layout/grid/col.stories.tsx
@@ -7,7 +7,7 @@ import { Row } from './row';
 
 /**
  * <a href="https://www.tedi.ee/1ee8444b7/p/24da19-grid" target="_BLANK">Zeroheight ↗</a><br/>
- * <a hreg="https://getbootstrap.com/docs/5.1/layout/grid" target="_BLANK">Boostrap docs ↗</a> <br/>
+ * <a hreg="https://getbootstrap.com/docs/5.1/layout/grid" target="_BLANK">Bootstrap docs ↗</a> <br/>
  * Row and Col components are inspired by Bootstrap V5 Grid System. <br/> You can use different Bootstrap grid classes
  * through component props.
  */
