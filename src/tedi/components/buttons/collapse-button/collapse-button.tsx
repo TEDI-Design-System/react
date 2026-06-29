fb87876eb5ee286ac1657335a0b469e51800c0a7
Airike Jaska <95303654+airikej@users.noreply.github.com>
2026-06-29 15:32:19 +0300

    feat(collapse): fix footer collapse alignment #687

--- src/tedi/components/buttons/collapse-button/collapse-button.tsx
+++ src/tedi/components/buttons/collapse-button/collapse-button.tsx
@@ -132,7 +132,13 @@ const CollapseButtonComponent = React.forwardRef<HTMLButtonElement, CollapseButt
             />
           </span>
         ) : (
-          <Icon className={styles['tedi-collapse-button__icon']} name="expand_more" color="inherit" filled size={24} />
+          <Icon
+            className={styles['tedi-collapse-button__icon']}
+            name="expand_more"
+            color="inherit"
+            filled
+            size={size === 'small' ? 18 : 24}
+          />
         )
       ) : (
         <span className={styles['tedi-collapse-button__icon-pad']}>
