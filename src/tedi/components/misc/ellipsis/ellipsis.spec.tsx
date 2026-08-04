27e1b8053257de93f831b4bcb17c5443b2bccf37
Airike Jaska <95303654+airikej@users.noreply.github.com>
2026-08-04 11:55:23 +0300

    fix(ellipsis): accessibility fix, cr fixes #752

--- src/tedi/components/misc/ellipsis/ellipsis.spec.tsx
+++ src/tedi/components/misc/ellipsis/ellipsis.spec.tsx
@@ -53,28 +53,27 @@ describe('Ellipsis Component', () => {
     jest.spyOn(HTMLElement.prototype, 'scrollHeight', 'get').mockReturnValue(100);
     jest.spyOn(HTMLElement.prototype, 'clientHeight', 'get').mockReturnValue(50);
 
-    const { container } = render(
+    render(
       <Ellipsis>
         <p>Ellipsis content</p>
       </Ellipsis>
     );
 
-    expect(screen.getByText('Ellipsis content')).toBeInTheDocument();
-    expect(container.querySelector('[data-name="ellipsis"]')).toHaveAttribute('role', 'button');
+    expect(screen.getByRole('button')).toHaveTextContent('Ellipsis content');
   });
 
   it('does not show popover when content fits within container', () => {
     jest.spyOn(HTMLElement.prototype, 'scrollHeight', 'get').mockReturnValue(50);
     jest.spyOn(HTMLElement.prototype, 'clientHeight', 'get').mockReturnValue(50);
 
-    const { container } = render(
+    render(
       <Ellipsis>
         <p>Ellipsis content</p>
       </Ellipsis>
     );
 
     expect(screen.getByText('Ellipsis content')).toBeInTheDocument();
-    expect(container.querySelector('[data-name="ellipsis"]')).not.toHaveAttribute('role');
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 
   it('does not apply a lineClamp style for the start position', () => {
