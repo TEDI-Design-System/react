12cf5369b3d9c915ce420634295ce15e476b0568
Airike Jaska <95303654+airikej@users.noreply.github.com>
2026-08-07 09:47:41 +0300

    fix(input-group): code review fixes #798

--- src/tedi/components/form/input-group/index.ts
+++ src/tedi/components/form/input-group/index.ts
@@ -1,5 +1,4 @@
 export * from './input-group';
-export { default as InputGroup } from './input-group';
 export * from './components/prefix/prefix';
 export * from './components/suffix/suffix';
 export * from './components/input/input';
